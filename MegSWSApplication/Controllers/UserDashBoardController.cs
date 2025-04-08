a55c2402a5d98ad058e4d98b34a8e8c1252a192a
SivaDhanush_K <sivadhanushkosuri9966@gmail.com>
2025-04-08 18:19:04 +0530

    userdashboard added

--- MegSWSApplication/Controllers/UserDashBoardController.cs
+++ MegSWSApplication/Controllers/UserDashBoardController.cs
@@ -1,13 +1,40 @@
 ﻿using MegSWSApplication.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace MegSWSApplication.Controllers
 {
     public class UserDashBoardController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserDashBoardController(IHttpContextAccessor httpContextAccessor)
         {
-            return View();
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Investors> investors = new List<Investors>();
+            var token = _httpContextAccessor.HttpContext.Session.GetString("JWToken");
+            var userId = _httpContextAccessor.HttpContext.Session.GetString("UserId");
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                // 🔁 Replace URL below with actual endpoint
+                var response = await client.GetAsync($"https://your-api-url/api/UserReg/GetInvestors?userId={userId}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    investors = JsonConvert.DeserializeObject<List<Investors>>(json);
+                }
+            }
+
+            return View(investors);
         }
     }
 }
