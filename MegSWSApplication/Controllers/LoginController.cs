51cd56ab7a4d4248e04d0df5d37566e2b6fab117
stellaesthers <stellaesther2024@gmail.com>
2025-04-09 11:43:02 +0530

    ForgotPassword

--- MegSWSApplication/Controllers/LoginController.cs
+++ MegSWSApplication/Controllers/LoginController.cs
@@ -58,7 +58,10 @@ namespace MegSWSApplication.Controllers
             return View(model);
         }
 
-
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
 
     }
 }
