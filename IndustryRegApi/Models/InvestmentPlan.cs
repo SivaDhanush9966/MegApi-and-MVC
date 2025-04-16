926cb5eb23da1f32b4a5ad7368065aa8268624e5
stellaesthers <stellaesther2024@gmail.com>
2025-04-16 11:49:33 +0530

    ModelForIndustryDetailsInvestmentPlan

---
+++ IndustryRegApi/Models/InvestmentPlan.cs
@@ -0,0 +1,16 @@
+﻿namespace IndustryRegApi.Models
+{
+    public class InvestmentPlan
+    {
+        public int IbpId { get; set; }
+        public int IbpUnitId { get; set; }
+        public int IbpInvesterId { get; set; }
+        public decimal Year1 { get; set; }
+        public decimal Year2 { get; set; }
+        public decimal Year3 { get; set; }
+        public decimal Year4 { get; set; }
+        public decimal Year5 { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int MrpId { get; set; }
+    }
+}
