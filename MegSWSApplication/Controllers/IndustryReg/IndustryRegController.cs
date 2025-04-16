0b23dc8179633ba0b8893fbf2c1fdb259c126aa7
SivaDhanush_K <sivadhanushkosuri9966@gmail.com>
2025-04-16 15:09:13 +0530

    view creatd

---
+++ MegSWSApplication/Controllers/IndustryReg/IndustryRegController.cs
@@ -0,0 +1,13 @@
+﻿using MegSWSApplication.Models.IndusRegModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MegSWSApplication.Controllers.IndustryReg
+{
+    public class IndustryRegController : Controller
+    {
+        public IActionResult BasicDetails(IndustryDetails model)
+        {
+            return View(model);
+        }
+    }
+}
