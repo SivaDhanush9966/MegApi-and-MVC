5f209f346e38f3412d65e15cd2f5796653e73322
jasika99 <jasikasaha99@gmail.com>
2025-04-17 10:58:15 +0530

    Industry Registration Enclosures

---
+++ MegSWSApplication/Controllers/IndustryReg/IndustryRegEnclosuresController.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace MegSWSApplication.Controllers.IndustryReg
+{
+    public class IndustryRegEnclosuresController : Controller
+    {
+        public IActionResult IndustryRegEnclosuresIdx()
+        {
+            return View();
+        }
+    }
+}
