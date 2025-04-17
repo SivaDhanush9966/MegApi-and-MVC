655760e3b6217dfcae97a5ffdb29422accd6501c
kamaji_saikrishna <kamaji_saikrishna@KAMAJISAIKRISHN>
2025-04-17 00:19:39 -0700

    MVC MASTER DTOs

---
+++ MegSWSApplication/DTO/Master/ConstitutionTypeDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MegSWSApplication.DTO.Master
+{
+    public class ConstitutionTypeDTO
+    {
+        public int ConstId { get; set; }
+        public string ConstType { get; set; }
+    }
+}
---
+++ MegSWSApplication/DTO/Master/StateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MegSWSApplication.DTO.Master
+{
+    public class StateDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
