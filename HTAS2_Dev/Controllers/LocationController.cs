7ab8f1df2f04e8814d26bbc6dad5cbe4999000ed
SivaDhanush_K <sivadhanushkosuri9966@gmail.com>
2025-04-22 16:41:36 +0530

    state contriller

--- HTAS2_Dev/Controllers/LocationController.cs
+++ HTAS2_Dev/Controllers/LocationController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HTAS2_Dev.BussinessLayer.Interface;
+using HTAS2_Dev.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HTAS2_Dev.Controllers
@@ -7,5 +9,25 @@ namespace HTAS2_Dev.Controllers
     [ApiController]
     public class LocationController : ControllerBase
     {
+        private readonly IStateMasterService _stateMasterService;
+
+        public LocationController(IStateMasterService stateMasterService)
+        {
+            _stateMasterService = stateMasterService;
+        }
+
+        [HttpGet("states")]
+        public IActionResult GetAllStates()
+        {
+            var states = _stateMasterService.GetAllStates();
+
+            if (states == null || states.Count == 0)
+            {
+                return NotFound("No states found.");
+            }
+
+            return Ok(states);
+        }
     }
 }
+
--- HTAS2_Dev/Program.cs
+++ HTAS2_Dev/Program.cs
@@ -1,5 +1,8 @@
+using HTAS2_Dev.BussinessLayer.Implementation;
+using HTAS2_Dev.BussinessLayer.Interface;
 using HTAS2_Dev.DataLayer.Implementation;
 using HTAS2_Dev.DataLayer.Interface;
+using HTAS2_Dev.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +12,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IStateMasterRepository, StateMasterRepository>();
+builder.Services.AddScoped<IStateMasterService, StateMasterService>();
+
 builder.Services.AddScoped<IRoleService, RoleRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
