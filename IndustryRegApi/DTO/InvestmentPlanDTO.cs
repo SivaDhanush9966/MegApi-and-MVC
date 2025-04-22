40af9cbf3975771a2f3ffa13adce64969a0ba43c
stellaesthers <stellaesther2024@gmail.com>
2025-04-22 11:55:22 +0530

    DTOIndustryRegAPI

--- IndustryRegApi/DTO/InvestmentPlanDTO.cs
+++ IndustryRegApi/DTO/InvestmentPlanDTO.cs
@@ -4,15 +4,13 @@ namespace IndustryRegApi.DTO
 {
     public class InvestmentPlanDTO: InvestmentPlan
     {
-        public int IbpId { get; set; }
-        public int IbpUnitId { get; set; }
-        public int IbpInvesterId { get; set; }
+     
+        public string ItemDescription { get; set; }
         public decimal Year1 { get; set; }
         public decimal Year2 { get; set; }
         public decimal Year3 { get; set; }
         public decimal Year4 { get; set; }
         public decimal Year5 { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public int MrpId { get; set; }
+      
     }
 }
